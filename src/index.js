fe0118fb186cf0803964a14bb8eb7137f4a89f44
dwg1207 <tranduong12072001@gmail.com>
2023-03-09 22:59:03 +0700

    Refactor code: create router, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const path = require('path');
 const express = require('express');
-const { engine } = require ('express-handlebars');
 const morgan = require('morgan');
+const { engine } = require ('express-handlebars');
 const app = express();
 const port = 3000
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.use(express.json());
+
+
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 //teamplate engine
 app.engine('.hbs', engine({ 
@@ -17,14 +25,11 @@ app.engine('.hbs', engine({
 app.set('view engine', "hbs");
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+//routes init
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
