e2e392f3ff2a3119b477b04e2a61ad74781c93f3
dwg1207 <tranduong12072001@gmail.com>
2023-03-19 09:35:16 +0700

    Delete course

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1/f8_blog_dev');
+        await mongoose.connect('mongodb://127.0.0.1/f8_blog_dev', {
+            // useNewUrlParser: true,
+            // userUnifiedTopology: true,
+            // useCreateIndex: true,
+        });
 
         console.log('connect successfully !!!');
-    } catch (err) {
+    } catch (error) {
         console.log('connect fail !!!');
     }
 }
